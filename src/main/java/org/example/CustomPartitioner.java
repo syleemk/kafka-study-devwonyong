342f93c1781ec9a3174dc8880f314cbb5e592b84
이수영/LastMile <sooyoung.lee@wesang.com>
2025-07-30 17:30:20 +0900

    feat: add custom partitioner

---
+++ src/main/java/org/example/CustomPartitioner.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import org.apache.kafka.clients.producer.Partitioner;
+import org.apache.kafka.common.Cluster;
+import org.apache.kafka.common.InvalidRecordException;
+import org.apache.kafka.common.PartitionInfo;
+import org.apache.kafka.common.utils.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Map;
+
+public class CustomPartitioner implements Partitioner {
+    private final static Logger logger = LoggerFactory.getLogger(CustomPartitioner.class);
+
+    @Override
+    public int partition(String topic, Object key, byte[] keyBytes, Object value, byte[] valueBytes, Cluster cluster) {
+        if (keyBytes == null) {
+            throw new InvalidRecordException("Need Message Key");
+        }
+
+        if (((String) key).equals("Pangyo"))
+            return 0;
+
+        List<PartitionInfo> partitions = cluster.partitionsForTopic(topic);
+        int numPartitions = partitions.size();
+        logger.info("Partition count for topic {}: {}", topic, numPartitions);
+        return Utils.toPositive(Utils.murmur2(keyBytes)) % numPartitions;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void configure(Map<String, ?> map) {
+
+    }
+}
