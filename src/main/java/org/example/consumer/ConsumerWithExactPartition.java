22977c387d96bd48707b160184b4a6906e4b62df
이수영/LastMile <sooyoung.lee@wesang.com>
2025-07-31 13:25:41 +0900

    feat: add exact partition consumer

---
+++ src/main/java/org/example/consumer/ConsumerWithExactPartition.java
@@ -0,0 +1,39 @@
+package org.example.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class ConsumerWithExactPartition {
+    private final static Logger logger = LoggerFactory.getLogger(ConsumerWithSyncCommit.class);
+    private final static String TOPIC_NAME = "test";
+    private final static String BOOTSTRAP_SERVERS = "localhost:9092";
+    private final static int PARTITION_NUMBER = 2;
+
+    public static void main(String[] args) {
+        Properties configs = new Properties();
+        configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+//        configs.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID); // consumer group id is not required for exact partition consumption
+        configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(configs);
+        consumer.assign(Collections.singleton(new TopicPartition(TOPIC_NAME, PARTITION_NUMBER)));
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
+            for (ConsumerRecord<String, String> record : records) {
+                logger.info("record:{}", record);
+            }
+        }
+    }
+}
