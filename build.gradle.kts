625b35e0edb0275f42e99002bb1630e7a3f80a32
Swim Lee <syleemk@gmail.com>
2025-08-12 02:19:56 +0900

    feat: add connect api dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,5 +12,6 @@ repositories {
 dependencies {
     implementation("org.apache.kafka:kafka-clients:2.5.0")
     implementation("org.apache.kafka:kafka-streams:3.2.0")
+    implementation("org.apache.kafka:connect-api:2.5.0")
     implementation("org.slf4j:slf4j-simple:1.7.30")
 }
